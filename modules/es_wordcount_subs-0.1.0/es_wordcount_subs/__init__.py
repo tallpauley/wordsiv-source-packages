d09685f89c32c074464d3d5d0fcf4dd3974a4d85
José Solé <type@jmsole.cl>
2023-12-03 15:29:29 +0000

    Spanish wordcount model

---
+++ modules/es_wordcount_subs-0.1.0/es_wordcount_subs/__init__.py
@@ -0,0 +1,32 @@
+import pkgutil
+from wordsiv.sentence_models_sources import (
+    WordCountSource,
+    SequentialModel,
+    RandomModel,
+)
+from pathlib import Path
+import json
+
+# Assuming installed as directory (zip_safe=False)
+HERE = Path(__file__).parent.absolute()
+
+# meta.json will be copied into the module by setup.py
+with open(HERE / "meta.json", "r") as f:
+    meta = json.load(f)
+
+# Sources should always be prefixed with the package name
+# as they will be merged into a common namespace
+sources = {
+    "es_wordcount_subs": {
+        "source": WordCountSource(HERE / "data" / "data.lfs.txt", meta, 50000),
+        "default_model_class": RandomModel,
+    },
+    "es_wordcount_subs_sm": {
+        "source": WordCountSource(HERE / "data" / "data.lfs.txt", meta, 10000),
+        "default_model_class": RandomModel,
+    },
+    "es_wordcount_subs_lg": {
+        "source": WordCountSource(HERE / "data" / "data.lfs.txt", meta),
+        "default_model_class": RandomModel,
+    },
+}
